58ec3b44614666fec7f438123ce95def07f0e2cf
Lochan Melam <lochan99melam@gmail.com>
2022-09-27 15:43:54 +0530

    Update 1388B.py

--- 1388B.py
+++ 1388B.py
@@ -1,8 +1,8 @@
 # modified the code in a better way
 def solve:
     for _ in range(int(input())):
-        m1=int(input())
-        x=(m1+3)//4
-        print("9"*(m1-x)+"8"*(x))
+        n=int(input())
+        x=(n+3)//4
+        print("9"*(n-x)+"8"*(n))
  
 solve();
